364b5e2780f20b2a03846802de544ec5df629826
TotpalIstvan <totpal.istvan@petrik.hu>
2021-10-22 22:28:26 +0200

    Kész feladat

--- src/hu/petrik/szuperhosprojekt/Main.java
+++ src/hu/petrik/szuperhosprojekt/Main.java
@@ -3,6 +3,25 @@ package hu.petrik.szuperhosprojekt;
 public class Main {
 
     public static void main(String[] args) {
+        Bosszuallo b = new Bosszuallo(15, false) {
 
+            @Override
+            public boolean megmentiAVilagot() {
+                return false;
+            }
+
+            @Override
+            public boolean legyoziE(Szuperhos szuperhos) {
+                return false;
+            }
+        };
+        Vasember v = new Vasember();
+        Batman bruce = new Batman();
+        System.out.println(b);
+        System.out.println(v);
+        System.out.println(bruce);
+        System.out.println("\n\n");
+        Kepregeny.szereplok("szuperhos.txt");
+        Kepregeny.szuperhosok();
     }
 }
